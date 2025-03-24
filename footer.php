24eb15996da07c145c2d37a7ed87ebbb2431a78b
fernandomartin04 <fernandomartin04@iesamachado.org>
2025-03-24 14:24:34 +0100

    header y footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+    <!-- Bootstrap JS (con Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9r+I6Jz8M+9O62Lwxz6Z5aBq5IhflP0r/N4M/IlxNuZDjw+" crossorigin="anonymous"></script>
+</body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,12 @@
+<?php include "db.php"; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Proyecto</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZjc+LdqscC7fYz9Z85lG32fw7Yw95IOatcPjqY2uTw9N/cbgbL6Y9MNo1h1+" crossorigin="anonymous">
+</head>
+<body>
